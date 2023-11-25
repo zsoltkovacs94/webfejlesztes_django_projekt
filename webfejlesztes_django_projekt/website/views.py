1095cfbd41282c25913447fd40d7d8d7706ab460
zsoltkovacs94 <zsoltkovacs94@mailbox.unideb.hu>
2023-11-25 17:05:09 +0100

    House tábla törlés és szerkesztés megvalósítása.

--- webfejlesztes_django_projekt/website/views.py
+++ webfejlesztes_django_projekt/website/views.py
@@ -10,6 +10,10 @@ def home(request):
 
 
 def persons(request):
+    if request.GET.get('mode'):
+        mode = request.GET['mode']
+    else:
+        mode = 'add'
     if request.method == 'POST' and 'createPerson' in request.POST:
         if House.objects.filter(id=request.POST.get('house')).count() == 0:
             print("Nem létező házhoz rendelés")
@@ -25,19 +29,42 @@ def persons(request):
                               name=request.POST.get('name'),
                               gender=request.POST.get('gender'),
                               birth=birthDate)
-        return HttpResponseRedirect(reverse("persons"))
-    return render(request, 'persons.html', {'data': Person.objects.all()})
+    return render(request, 'persons.html', {'data': Person.objects.all(),
+                                            'mode': mode})
 
 
 def houses(request):
+    if request.GET.get('mode'):
+        mode = request.GET['mode']
+    else:
+        mode = 'add'
     if request.method == 'POST' and 'createHouse' in request.POST:
         if House.objects.filter(address=request.POST.get('address')).count() != 0:
             print("Unique constraint megsértése")
             #Későbbiekben helyettesíteni
             return HttpResponseRedirect(reverse("houses"))
         House.objects.create(address=request.POST.get('address'), houseType=request.POST.get('houseType'))
-        return HttpResponseRedirect(reverse("houses"))
-    return render(request, 'houses.html', {'data': House.objects.all()})
+    if request.method == 'POST' and 'editHouse' in request.POST:
+        if House.objects.filter(id=request.POST.get('id')).count() == 0:
+            print("Nem található az adott ház")
+            #Későbbiekben helyettesíteni
+            return HttpResponseRedirect(reverse("houses"))
+        newAddress = request.POST.get('address')
+        newHT = request.POST.get('houseType')
+        if newAddress == '' and newHT != '':
+            House.objects.filter(id=request.POST.get('id')).update(houseType=newHT)
+        elif newHT == '' and newAddress != '':
+            House.objects.filter(id=request.POST.get('id')).update(address=newAddress)
+        elif newHT == '' and newAddress == '':
+            print("Üres edit")
+            #Későbbiekben helyettesíteni
+        else:
+            House.objects.filter(id=request.POST.get('id')).update(address=newAddress,
+                                                                   houseType=newHT)
+    if request.method == 'POST' and 'deleteHouse' in request.POST:
+        House.objects.filter(id=request.POST.get('id')).delete()
+    return render(request, 'houses.html', {'data': House.objects.all(),
+                                           'mode': mode})
 
 
 def addTestPerson(request):
