d16e3e2fb73074076a01ca61a2c87b889dbbd283
zsoltkovacs94 <zsoltkovacs94@mailbox.unideb.hu>
2023-11-25 10:40:07 +0100

    favicon.ico pótlása

--- webfejlesztes_django_projekt/webfejlesztes_django_projekt/urls.py
+++ webfejlesztes_django_projekt/webfejlesztes_django_projekt/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('', include('website.urls')),
+	path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
